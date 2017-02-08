f49cc6cb6a67f2a1eb9bd5d9567d08965ed27e1e
BURAK KURSAD GUNHAN <burak.gunhan@med.uni-goettingen.de>
2017-02-08 12:43:43 +0100

    Sparse pairwise meta-analysis dataset added

---
+++ R/KussHeartdat.R
@@ -0,0 +1,10 @@
+#' Data for the ischemic heart disease sparse pairwise meta-analysis discussed in Kuss (2014)
+#'
+#' Dataset for pairwise meta-analysis with many zero entries. Data is from a Cochrane review on
+#' postoperative stroke occurrence when comparing off-pump and on-pump coronary artery
+#' bypass grafting for ischemic heart disease. This data is used as an example in Kuss (2014).
+#'
+#' @source Kuss O. (2015), Statistical methods for meta-analyses including information
+#' from studies without any events--add nothing to nothing and succeed nevertheless,
+#' Statist. Med., 34; pages 1097-1116, doi: 10.1002/sim.6383
+"KussHeartdat"
